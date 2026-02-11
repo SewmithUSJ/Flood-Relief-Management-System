1ad2bfb2f4ee5b71b9a249b762a55c6ef9fd1ea4
Kaveesha-13 <kaveeshasandaru868@gmail.com>
2026-02-12 00:59:29 +0530

    add_user_sidebar

---
+++ frontend/user/user-navbar.php
@@ -0,0 +1,28 @@
+<?php
+ include "../config/header.php";
+ ?>
+    <aside class=" user-sidebar sidebar-bg d-flex flex-column">
+
+       
+        <div class="sidebar-logo text-center py-3">
+            <img src="../image/logo.jpeg" alt="Logo" class="img-fluid w-75">
+        </div>
+
+       
+        <ul class="nav flex-column px-2 mt-2">
+            <li class="nav-item">
+                <a class="nav-link text-white active" href="#">Profile</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light" href="#">Request</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-light" href="#">My Requests</a>
+            </li>
+            <li class="nav-item mt-3">
+                <a class="nav-link text-danger" href="#">Logout</a>
+            </li>
+        </ul>
+
+    </aside>
+ 
