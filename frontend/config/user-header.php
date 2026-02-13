5873846936d4d0f842c0731cbe809d780090bb4f
Kaveesha-13 <kaveeshasandaru868@gmail.com>
2026-02-14 00:20:09 +0530

    add sidebar

--- frontend/config/user-header.php
+++ frontend/config/user-header.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Flood-Relief-Management-System</title>
+    <title>Relief Request - User Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="../style/user.css">
 </head>
-
 <body>
-    
