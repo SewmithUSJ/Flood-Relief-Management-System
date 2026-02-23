5c30327a725f9cf20128586a55a03e5b2c7275a4
R G Yasindu sewmith <as20240555@sci.sjp.ac.lk>
2026-02-23 10:51:18 +0530

    add_slidbar_navbar

--- frontend/config/admin-header.php
+++ frontend/config/admin-header.php
@@ -7,7 +7,7 @@
     <title>Flood-Relief-Management-System</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/admin.css">
 </head>
 
 <body>
